1335e93d69c7651da56bdc364a0e6e5541ac4e3f
chetanyed <122827668+chetanyed@users.noreply.github.com>
2023-03-17 21:41:21 +0530

    Add files via upload

---
+++ divineguidaance.js
@@ -0,0 +1,17 @@
+document.getElementById('butt1').addEventListener("click",myfunction);
+function myfunction(){
+    var c=document.getElementById('checkbox');
+
+    if (c.checked==true){
+       c.checked=false;
+       
+        
+    
+    }
+    else{
+     c.checked=true;
+     
+    }
+}
+
+            
